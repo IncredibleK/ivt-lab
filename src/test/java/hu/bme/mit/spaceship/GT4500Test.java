3e5d3a557b2ee2a9e74865bad653432f3beaf000
IncredibleK <aron11111@gmail.com>
2019-05-10 09:46:28 +0200

    Negyedik es otodik feladat

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -23,11 +23,8 @@ public class GT4500Test {
   public void fireTorpedo_Single_Success(){
     // Arrange
 	when(primary.getTorpedoCount()).thenReturn(10);
-	when(secondary.getTorpedoCount()).thenReturn(10);
 	when(primary.isEmpty()).thenReturn(false);
-	when(secondary.isEmpty()).thenReturn(false);
 	when(primary.fire(1)).thenReturn(true);
-	when(secondary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
@@ -36,6 +33,89 @@ public class GT4500Test {
     assertEquals(true, result);
 	verify(primary,times(1)).fire(1);
 	verify(secondary,times(0)).fire(1);
+  }
+  
+  @Test
+  public void fireTorpedo_FirstCase(){
+   // Arrange
+   	when(primary.getTorpedoCount()).thenReturn(0);
+    when(secondary.getTorpedoCount()).thenReturn(10);
+	when(primary.isEmpty()).thenReturn(true);
+	when(secondary.isEmpty()).thenReturn(false);
+	when(secondary.fire(1)).thenReturn(true);
+  
+   // Act
+   boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+   
+   // Assert
+   assertEquals(true, result);
+	verify(primary,times(0)).fire(1);
+	verify(secondary,times(1)).fire(1);
+   
+  }
+  
+  @Test
+  public void fireTorpedo_SecondCase(){
+   // Arrange
+	when(primary.isEmpty()).thenReturn(false);
+	when(secondary.isEmpty()).thenReturn(true);
+	when(primary.fire(1)).thenReturn(true);
+   // Act
+   boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+   boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+   
+   // Assert
+    assertEquals(true, result);
+	verify(primary,times(2)).fire(1);
+	verify(secondary,times(0)).fire(1);
+  }
+  
+  @Test
+  public void fireTorpedo_ThirdCase(){
+   // Arrange
+	when(primary.isEmpty()).thenReturn(false);
+	when(secondary.isEmpty()).thenReturn(false);
+	when(primary.fire(1)).thenReturn(true);
+	when(primary.fire(1)).thenReturn(true);
+   // Act
+   boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+   boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+   
+   // Assert
+    assertEquals(true, result);
+	verify(primary,times(1)).fire(1);
+	verify(secondary,times(1)).fire(1);
+  }
+  
+  @Test
+  public void fireTorpedo_FourthCase(){
+   // Arrange
+  when(primary.isEmpty()).thenReturn(true);
+	when(secondary.isEmpty()).thenReturn(true);
+  
+   // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+   
+   // Assert
+   assertEquals(false, result);
+	verify(primary,times(0)).fire(1);
+	verify(secondary,times(0)).fire(1);
+   
+  }
+  
+  @Test
+  public void fireTorpedo_FifthCase(){
+   /// Arrange
+  when(primary.isEmpty()).thenReturn(true);
+	when(secondary.isEmpty()).thenReturn(true);
+  
+   // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+   
+   // Assert
+   assertEquals(false, result);
+   verify(primary,times(0)).fire(1);
+   verify(secondary,times(0)).fire(1);
   }
 
   @Test
